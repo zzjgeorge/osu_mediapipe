db5ec6861779969d215b4983e9ba23016fa4bff3
unknown <zzjgeorge@github.com>
2021-10-21 01:21:27 +0900

    implemented threading to improve speed

--- scene_handler.py
+++ scene_handler.py
@@ -7,6 +7,8 @@ import cv2
 import numpy as np
 from PIL import Image
 import mediapipe as mp
+import threading
+import copy
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -44,31 +46,16 @@ def update_circle(dt):
             break
         elif t > gen_time + T_END:
             hit_sound.play()
-            hitpoint['shape'].delete()
-            hitpoint['shape2'].delete()
+            hitpoint['inner'].delete()
+            hitpoint['outer'].delete()
             i1 += 1
-            print(i1)
             continue
         else:
             x, y = int(hitpoint['x'])/osu_w*WIDTH+100, int(hitpoint['y'])/osu_h*HEIGHT+50
-            hitpoint['shape2'] = pyglet.shapes.Circle(x, y, RADIUS*(1+(gen_time-t)/2000), batch=outer_batch)
-            hitpoint['shape2'].opcatiy = 50
-            hitpoint['shape'] = pyglet.shapes.Circle(x, y, RADIUS, color=(255, 255, 0), batch=circle_batch)
+            hitpoint['outer'] = pyglet.shapes.Circle(x, y, RADIUS*(1+(gen_time-t)/2000), batch=outer_batch)
+            hitpoint['inner'] = pyglet.shapes.Circle(x, y, RADIUS, color=(255, 255, 0), batch=circle_batch)
 
-# pImg=None
-# g_CamTex = glGetIntegerv(GL_TEXTURE_BINDING_2D)
-# def camera(dt):
-#     global pImg
-#     # find_finger_pos()
-#     frame = cap.read(0)
-#     cv2.cvtColor(frame, frame, cv2.COLOR_BGR2RGB)
-#     glBindTexture(GL_TEXTURE_2D, g_CamTex)
-#     glTexSubImage2D(GL_TEXTURE_2D, 0,
-#                     (WIDTH - frame.cols)/2,
-#                     (HEIGHT - frame.rows)/2,
-#                     frame.cols, frame.rows,
-#                     GL_RGB, GL_UNSIGNED_BYTE, frame.data)
-#     pImg = pyglet.image.ImageData(WIDTH, HEIGHT, 'BGR', g_CamTex)
+
 def cv2glet(img):
     '''Assumes image is in BGR color space. Returns a pyimg object'''
     rows, cols, channels = img.shape
@@ -87,9 +74,46 @@ def cv2glet(img):
 
 
 cap = cv2.VideoCapture(0)
-image = cap.read()
-image = image[1]
-pyimg = cv2glet(image)
+frame_data = cap.read()
+frame_data = frame_data[1]
+pyimg = cv2glet(frame_data)
+
+def thread_mediapipe():
+    global frame_data
+    cap = cv2.VideoCapture(0)
+    with mp_hands.Hands(
+        model_complexity=0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = hands.process(image)
+            # Draw the hand annotations on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        image,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style())
+            if cv2.waitKey(5) & 0xFF == 27:
+                print("exiting")
+                break
+            frame_data = image.copy()
+    cap.release()
+    exit(0)
+
 
 def find_finger_pos(dt):
     global tip, mcp, image
@@ -120,10 +144,11 @@ def find_finger_pos(dt):
                 #     mp_drawing_styles.get_default_hand_connections_style())
                 tip = [hand_landmarks.landmark[8].x, hand_landmarks.landmark[8].y,hand_landmarks.landmark[8].z]
                 mcp = [hand_landmarks.landmark[0].x, hand_landmarks.landmark[0].y,hand_landmarks.landmark[0].z]
+        
 
 def update_camera(dt):
     global pyimg
-    raw_frame = image
+    raw_frame = frame_data
     frame = cv2.resize(raw_frame, None, fx=2, fy=2)
     frame = cv2.flip(frame, 1)
     pyimg = cv2glet(frame)
@@ -136,8 +161,17 @@ def on_draw():
     outer_batch.draw()
     circle_batch.draw()
     
+@window.event()
+def on_close():
+    print("quitting")
+    t.join()
+    pyglet.app.EventLoop().exit()
+    return pyglet.event.EVENT_HANDLED
+
 bgm.play()
+t = threading.Thread(target = thread_mediapipe, name='mediapipe')
+t.start()
 #pyglet.clock.schedule_interval(find_finger_pos, 1)
-pyglet.clock.schedule_interval(update_camera, 1/24)
+pyglet.clock.schedule_interval(update_camera, 1/60)
 pyglet.clock.schedule_interval(update_circle, 1/100)
 pyglet.app.run()
